f4f37883d5f92f2a1d49a1bb8a7b225712d8a5b8
Jonas yoney <nickgoge1990@gmail.com>
2023-09-18 19:58:23 +0100

    Fixed buttons

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from flask import Flask, render_template
-
+from blueprints import blueprints
 
 def start():
-  app = Flask("app")
+    app = Flask("app")
 
-  if app == None: return
+    if app == None: return
 
-  @app.route("/")
-  def main():
-    return render_template("main.html")
+    app.register_blueprint(blueprints)
 
-  @app.route("/downloads")
-  def downloads():
-    return render_template("downloads.html")
-
-  @app.route("/about")
-  def about():
-    return render_template("about.html")
-
-  app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":
-  start()
+    start()
